69de0c860c650606226feb91c6c4d6ff224b4609
Hugo Girard <hugirard@microsoft.com>
2025-01-06 11:48:29 -0500

    upload info state

--- scripts/terraform-plan-apply.sh
+++ scripts/terraform-plan-apply.sh
@@ -147,6 +147,7 @@ if (( ${#OUTPUT_FILES[@]} ));
 then
     printInfo "Writing terraform output to ${OUTPUT_FILES[*]}"
     terraform output -json | tee ${OUTPUT_FILES[*]} > /dev/null
-    printInfo "Writting in directory ${DIR}"
-    
+    az login --service-principal --username $CLIENT_ID --password $CLIENT_SECRET --tenant $TENANT_ID
+    echo "This is a dummy file" > dummyfile.txt
+    az storage blob upload --account-name strtfstatehg --container-name output --name inf_output.json  --file inf_output.json  --auth-mode login    
 fi
